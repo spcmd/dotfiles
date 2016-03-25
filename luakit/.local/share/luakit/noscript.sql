f3ea8f8fa6562831bf1a5f36b71ee1a5f2738092
spcmd <spcmdgit@gmail.com>
2016-03-25 18:16:58 +0100

    initial commit

---
+++ luakit/.local/share/luakit/noscript.sql
@@ -0,0 +1,18 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE by_domain (
+    id INTEGER PRIMARY KEY,
+    domain TEXT,
+    enable_scripts INTEGER,
+    enable_plugins INTEGER
+);
+INSERT INTO "by_domain" VALUES(4,'bookmarks',1,0);
+INSERT INTO "by_domain" VALUES(5,'downloads',1,0);
+INSERT INTO "by_domain" VALUES(6,'adblock',1,0);
+INSERT INTO "by_domain" VALUES(7,'duckduckgo.com',1,0);
+INSERT INTO "by_domain" VALUES(9,'history',1,0);
+INSERT INTO "by_domain" VALUES(13,'help',1,0);
+INSERT INTO "by_domain" VALUES(14,'www.youtube.com',1,0);
+INSERT INTO "by_domain" VALUES(15,'m.youtube.com',1,0);
+INSERT INTO "by_domain" VALUES(16,'www.imdb.com',1,0);
+COMMIT;
