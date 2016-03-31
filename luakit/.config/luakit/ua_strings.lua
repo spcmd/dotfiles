70e03f0669f6f481cc17add20c67a5f3aa090f9f
spcmd <spcmdgit@gmail.com>
2016-03-31 16:55:48 +0200

    user agent switch without plugins dir and configs

---
+++ luakit/.config/luakit/ua_strings.lua
@@ -0,0 +1,5 @@
+uaswitch.ua_strings.fakes = {
+
+        ["firefox"]         = "Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0",
+        ["chrome"]          = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+}
