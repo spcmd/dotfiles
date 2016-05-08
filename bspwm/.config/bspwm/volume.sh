27b0e2e92687c0a0e81419c00bd6b027bbbaba58
spcmd <spcmdgit@gmail.com>
2016-05-08 19:10:02 +0200

    major updates

--- bspwm/.config/bspwm/volume.sh
+++ bspwm/.config/bspwm/volume.sh
@@ -7,4 +7,15 @@ case $1 in
     down)
         amixer sset Master 2%- ; echo "V$(amixer -M get Master | awk '/%/{gsub( /[][%]/,"" );print $5}' | head -n1)" > "$PANEL_FIFO"
         ;;
+    toggle)
+	amixer sset Master toggle
+	level=$(amixer -M get Master | awk '/%/{gsub( /[][%]/,"" );print $5}' | head -n1)
+	state=$(amixer -M get Master | awk '/\[on|off\]/{gsub("[\\[\\]]","");print $NF}' | head -n1)
+	if [[ $state = "off" ]]; then
+		level="%{F#ff3b3b}$level"
+	else
+		level=$level
+	fi
+	echo "V$level" > "$PANEL_FIFO"
+	;;
 esac
