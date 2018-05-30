c128f3528720afe4ce41f5352ebf3add3f3b527e
spcmd <spcmdgit@gmail.com>
2018-05-30 11:57:18 +0200

    minor updates

--- i3blocks/.config/i3blocks/i3blocks_battery.sh
+++ i3blocks/.config/i3blocks/i3blocks_battery.sh
@@ -4,11 +4,11 @@ capacity=$(cat /sys/class/power_supply/BAT0/capacity)
 status=$(cat /sys/class/power_supply/BAT0/status)
 
 if [ "$status" != "Charging" ] && [ "$capacity" -le 30 ] && [ "$capacity" -gt 20 ]; then
-        widget_cap="$capacity"
+        widget_cap="<span foreground='#ffe490'>$capacity%</span>"
 elif [ "$status" != "Charging" ] && [ "$capacity" -le 20 ]; then
-        widget_cap="$capacity"
+        widget_cap="<span foreground='#ff0000'>$capacity%</span>"
 else
-        widget_cap=$capacity
+        widget_cap="$capacity%"
 fi
 
 if [ "$status" = "Charging" ]; then
@@ -17,6 +17,6 @@ else
         widget_status="Bat"
 fi
 
-echo "${widget_status}: ${widget_cap}%"
+echo "${widget_status}: ${widget_cap}"
 
 
