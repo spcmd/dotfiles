861693103908739cd9eaaf76bd45ed42ab418ade
spcmd <spcmdgit@gmail.com>
2019-03-02 17:27:18 +0100

    widget update

--- i3blocks/.config/i3blocks/i3blocks_battery.sh
+++ i3blocks/.config/i3blocks/i3blocks_battery.sh
@@ -4,19 +4,19 @@ capacity=$(cat /sys/class/power_supply/BAT0/capacity)
 status=$(cat /sys/class/power_supply/BAT0/status)
 
 if [ "$status" != "Charging" ] && [ "$capacity" -le 30 ] && [ "$capacity" -gt 20 ]; then
-        #widget_cap="<span foreground='#ffe490'>$capacity%</span>"
-        widget_cap="<span foreground='#C21E00'>$capacity%</span>"
+
+	bat_widget="<span foreground='#C21E00'> $capacity%</span>"
+
 elif [ "$status" != "Charging" ] && [ "$capacity" -le 20 ]; then
-        #widget_cap="<span foreground='#ff0000'>$capacity%</span>"
-        widget_cap="<span foreground='#de0404'>$capacity%</span>"
+
+       bat_widget="<span foreground='#de0404'> $capacity%</span>"
+
+elif [ "$status" = "Charging" ] || [ "$status" = "Full" ]; then
+
+	bat_widget="  $capacity%"
+
 else
-        widget_cap="$capacity%"
+        bat_widget=" $capacity%"
 fi
 
-if [ "$status" = "Charging" ]; then
-        widget_status=""
-else
-        widget_status=""
-fi
-
-echo "${widget_status} ${widget_cap}"
+echo "${bat_widget}"
